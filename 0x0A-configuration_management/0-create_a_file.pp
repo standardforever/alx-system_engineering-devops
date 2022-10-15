202e544996825118b3c50ec579b653de94a1948c
standardforever <standard.forever123@gmail.com>
2022-10-14 21:45:02 -0700

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file in /tmp directory
+# Create a file in /tmp
 
 file { 'school':
-     content => 'I love Puppet',
-     group   => 'www-data',
-     mode    => '0744',
-     owner   => 'www-data',
-     path    => '/tmp/school',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
