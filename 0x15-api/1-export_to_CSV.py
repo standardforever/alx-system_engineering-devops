5d95f8188af7e11e5d30d2cc2985e47ca7593c25
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-11-14 13:47:44 -0500

    updated

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+export json file to csv
+"""
+
+import requests
+from os import sys
+import csv
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get("{}/users/{}".format(url, id)).json()
+    todos = requests.get("{}/todos".format(url), params={"userId": id}).json()
+
+    json_todo = {}
+    todo_list = []
+    for todo in todos:
+        del todo["id"]
+        todo["name"] = user.get("name")
+        todo_list.append(todo)
+    field_name = ["userId", "name", "completed", "title"]
+    with open("USER_ID.csv", 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=field_name)
+        writer.writerows(todo_list)
