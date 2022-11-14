89cd38a507595752bdb78c1f0d4cdf01329fce67
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-11-14 17:03:42 -0500

    updated

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     user = requests.get("{}/users/{}".format(url, id)).json()
     todos = requests.get("{}/todos".format(url), params={"userId": id}).json()
 
+    username = user.get("username")
     todo_dict = {}
     todo_list = []
     for todo in todos:
-        todo_dict["task"] = todo["title"]
-        todo_dict["completed"] = todo["completed"]
-        todo_dict["username"] = user["username"]
+        todo_dict["task"] = todo.get("title")
+        todo_dict["completed"] = todo.get("completed")
+        todo_dict["username"] = username
         todo_list.append(todo_dict)
     new_dict = {}
     new_dict[id] = todo_list
